c4307f858578f469ad549d826bfcdfe18bf741bc
Matthew Sun <sun3matthew@gmail.com>
2024-12-05 17:16:31 -0800

    FIX Three JS File Serving Convention

--- src/managers/textureManager.js
+++ src/managers/textureManager.js
@@ -7,14 +7,14 @@ export class TextureManager {
         const textureLoader = new THREE.TextureLoader();
 
         TextureManager.Textures = {
-            "B": textureLoader.load('../assets/textures/brick.png'),
-            "O": textureLoader.load('../assets/textures/oil.png'),
-            "G": textureLoader.load('../assets/textures/grill.png'),
-            "W": textureLoader.load('../assets/textures/water.png'),
+            "B": textureLoader.load('../textures/brick.png'),
+            "O": textureLoader.load('../textures/oil.png'),
+            "G": textureLoader.load('../textures/grill.png'),
+            "W": textureLoader.load('../textures/water.png'),
 
-            "P1": textureLoader.load('../assets/textures/lettuce.png'),
-            "P2": textureLoader.load('../assets/textures/bread.png'),
-            "background": textureLoader.load('../assets/textures/background.png'),
+            "P1": textureLoader.load('../textures/lettuce.png'),
+            "P2": textureLoader.load('../textures/bread.png'),
+            "background": textureLoader.load('../textures/background.png'),
         };
 
         for (const key in TextureManager.Textures) {
